db7ebb6fc503098a4e8f3ce72b1e7aa55040970a
Alexey Zabashta <gwynblayde@mail.ru>
2022-01-13 15:12:41 +0300

    feat: add solutions to numbers-tesks.js 13.01.2022 15:12

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -114,8 +114,14 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  if ((x1 === x2) && (y1 === y2)) {
+    return 0;
+  }
+  if ((x1 === x2) || (y1 === y2)) {
+    return Math.PI;
+  }
+  return Math.PI / 2;
 }
 
 /**
@@ -130,8 +136,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const dig = `${value}`.split('');
+  return Number(dig[dig.length - 1]);
 }
 
 
@@ -146,8 +153,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -163,8 +170,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a * a + b * b + c * c);
 }
 
 
@@ -185,8 +192,11 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) {
+    return num;
+  }
+  return (Math.round(num / 10 ** pow)) * 10 ** pow;
 }
 
 /**
@@ -206,8 +216,13 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -225,8 +240,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const out = Number.isNaN(Number(value));
+  if (out) {
+    return def;
+  }
+  return Number(value);
 }
 
 module.exports = {
